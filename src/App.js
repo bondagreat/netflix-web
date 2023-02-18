f571daad2d2967998ce05aeec19ff21b7520dda1
bondagreat <bon_thegreat@hotmail.com>
2023-02-19 02:12:45 +0700

    auth redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAuthUser } from './redux/authSlice';
+import { getAccessToken } from './utils/local-storage';
+
 function App() {
+  const dispatch = useDispatch();
+  const authUser = useSelector((state) => state.auth.user);
+
+  useEffect(() => {
+    if (getAccessToken()) {
+      dispatch(fetchAuthUser());
+    }
+  }, []);
+
   return (
     <div>
-      <span>Hello world</span>
+      <span>Hello {authUser ? authUser.email : 'world'}</span>
     </div>
   );
 }
