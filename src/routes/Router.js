a71552ec67baea51090ea051533cff6af248ab0c
SimpleJa2020 <ja_37@hotmail.co.th>
2023-02-20 01:27:51 +0700

    css profile

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -3,52 +3,61 @@ import ProtectedRoute from '../features/auth/ProtectedRoute';
 import RedirectIfAuthenticate from '../features/auth/RedirectIfAuthenticate';
 import AuthLayout from '../layouts/AuthLayout';
 import LoginPage from '../pages/LoginPage';
+import ProfilePage from '../pages/ProfilePage';
 import RegisterPage from '../pages/RegisterPage';
 
 const router = createBrowserRouter([
-  {
-    path: '/login',
-    element: (
-      <RedirectIfAuthenticate>
-        <LoginPage />
-      </RedirectIfAuthenticate>
-    ),
-  },
-  {
-    path: '/register',
-    element: (
-      <RedirectIfAuthenticate>
-        <RegisterPage />
-      </RedirectIfAuthenticate>
-    ),
-  },
-  {
-    element: (
-      <ProtectedRoute>
-        <AuthLayout />
-      </ProtectedRoute>
-    ),
-    children: [
-      {
-        path: '/browse',
-        element: <h1>Profile + home</h1>,
-      },
-      {
-        path: '/browse/latest',
-        element: <h1>New & Popular</h1>,
-      },
-      {
-        path: '/browse/my-list',
-        element: <h1>My List</h1>,
-      },
-      {
-        path: '/browse/original-audio',
-        element: <h1>Browse by Languages</h1>,
-      },
-    ],
-  },
+    {
+        path: '/login',
+        element: (
+            <RedirectIfAuthenticate>
+                <LoginPage />
+            </RedirectIfAuthenticate>
+        )
+    },
+    {
+        path: '/register',
+        element: (
+            <RedirectIfAuthenticate>
+                <RegisterPage />
+            </RedirectIfAuthenticate>
+        )
+    },
+    {
+        path: '/profile',
+        element: <ProfilePage />
+    },
+    {
+        path: '/ManageProfile',
+        element: <ProtectedRoute></ProtectedRoute>
+    },
+    {
+        element: (
+            <ProtectedRoute>
+                <AuthLayout />
+            </ProtectedRoute>
+        ),
+        children: [
+            {
+                path: '/browse',
+                element: <h1>Profile + home</h1>
+            },
+            {
+                path: '/browse/latest',
+                element: <h1>New & Popular</h1>
+            },
+            {
+                path: '/browse/my-list',
+                element: <h1>My List</h1>
+            },
+            {
+                path: '/browse/original-audio',
+                element: <h1>Browse by Languages</h1>
+            }
+        ]
+    }
 ]);
 
 export default function Router() {
-  return <RouterProvider router={router} />;
+    return <RouterProvider router={router} />;
 }
