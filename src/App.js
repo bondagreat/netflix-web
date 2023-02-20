dcc09104e12ab30c5851e52ea4f20d24c5193bb1
Yokkorn <suratipyok@gmail.com>
2023-02-20 12:24:58 +0700

    css-header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-<<<<<<< HEAD
-import Header from "./layouts/Header";
-=======
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchAuthUser } from './redux/authSlice';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAuthUser } from "./redux/authSlice";
 
-import useLoading from './hooks/useLoading';
-import Router from './routes/Router';
-import Spinner from './components/Spinner';
-import { getAccessToken } from './utils/local-storage';
->>>>>>> dev
+import useLoading from "./hooks/useLoading";
+import Router from "./routes/Router";
+import Spinner from "./components/Spinner";
+import { getAccessToken } from "./utils/local-storage";
+import Header from "./layouts/Header";
 
 function App() {
   const { loading } = useLoading();
 
   const dispatch = useDispatch();
-  const authUser = useSelector((state) => state.auth.user);
+  const authUser = useSelector(state => state.auth.user);
 
   useEffect(() => {
     if (getAccessToken()) {
@@ -24,16 +21,13 @@ function App() {
   }, []);
 
   return (
-<<<<<<< HEAD
-    <div>
-      <Header />
-    </div>
-=======
     <>
+      <div>
+        <Header />
+      </div>
       {loading && <Spinner />}
       <Router />
     </>
->>>>>>> dev
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import "tw-elements";
-=======
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import './index.css';
-import App from './App';
 import LoadingContextProvider from './contexts/LoadingContext';
->>>>>>> dev
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
