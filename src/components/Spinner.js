bd2c519544276d3416aeb44c4a98c08158e91b22
SimpleJa2020 <ja_37@hotmail.co.th>
2023-02-21 12:17:35 +0700

    finish edit and manage profile

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,17 +2,17 @@ import ClipLoader from 'react-spinners/ClipLoader';
 import useLoading from '../hooks/useLoading';
 
 export default function Spinner() {
-    const { loading } = useLoading();
+  const { loading } = useLoading();
 
-    return (
-        <div className="absolute w-screen h-screen bg-black z-[1000] flex justify-center items-center">
-            <ClipLoader
-                color="#ff0000"
-                loading={loading}
-                size={150}
-                aria-label="Loading Spinner"
-                data-testid="loader"
-            />
-        </div>
-    );
+  return (
+    <div className="absolute w-screen h-screen bg-black z-[1000] flex justify-center items-center">
+      <ClipLoader
+        color="#ff0000"
+        loading={loading}
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+    </div>
+  );
 }
