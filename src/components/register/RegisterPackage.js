1ff6ce31407eb331938404c8e60d0eacfa5e2b9b
bondagreat <bon_thegreat@hotmail.com>
2023-02-27 17:02:14 +0700

    fix auth 3

--- src/components/register/RegisterPackage.js
+++ src/components/register/RegisterPackage.js
@@ -1,4 +1,4 @@
-import { CheckCircleIcon, CheckIcon } from '../../images';
+import { CheckIcon } from '../../images';
 import PkMonthlyPic from '../../assets/images/pkmonthly.png';
 import CardMonthlyPic from '../../assets/images/cardmonthly.png';
 import CardThreeMPic from '../../assets/images/cardthreem.png';
@@ -60,10 +60,10 @@ export default function RegisterPackage() {
         <div className="flex justify-center">
           <div className="block p-6 rounded-lg shadow-lg border-gray-600/80 bg-white max-w-sm mt-30 mr-5">
             <div className="w-[100px] h-[100px]  rounded-md ml-32 mb-5">
-              <img src={PkMonthlyPic} />
+              <img src={PkMonthlyPic} alt="1" />
             </div>
             <div className="w-[350px]  rounded-md">
-              <img src={CardMonthlyPic} />
+              <img src={CardMonthlyPic} alt="1" />
             </div>
             <Link to={{ pathname: '/registerpay' }}>
               <button
@@ -77,10 +77,10 @@ export default function RegisterPackage() {
           </div>
           <div className="block p-6 rounded-lg shadow-lg border-gray-600/80 bg-white max-w-sm mt-30 ml-5">
             <div className="w-[100px] h-[100px]  rounded-md ml-32  mb-5">
-              <img src={PkThreeMPic} />
+              <img src={PkThreeMPic} alt="1" />
             </div>
             <div className="w-[350px]  rounded-md">
-              <img src={CardThreeMPic} />
+              <img src={CardThreeMPic} alt="1" />
             </div>
             <Link to={{ pathname: '/registerpay' }}>
               <button
