49d30722ac051c5d2de937840d18d12efd68c962
DESKTOP-6S97JAT\Nero <thanyaporn.ch688@gmail.com>
2023-03-04 03:51:42 +0700

    cacel member in the process two

--- src/components/account/CancelMemberShip.js
+++ src/components/account/CancelMemberShip.js
@@ -1,19 +1,23 @@
 import { ArrowLeftForm, ArrowRightForm, Dot } from '../../images';
 import Footer from '../../layouts/Footer';
 import NavBarAccount from '../../layouts/NavBarAccount';
+import MovieJunge from '../../assets/images/moviejung.png';
+import { Link } from 'react-router-dom';
 export default function CancelMemberShip() {
   return (
     <>
       <NavBarAccount />
-      <div className="flex-col  ml-[200px] mt-[90px]  mr-10 justify-center w-[1000px]">
-        <div className="flex mb-6">
-          <a className="text-blue-600 cursor-pointer no-underline hover:underline text-sm ">
-            Return to Netflix
-          </a>
-        </div>
-        <div className=" mb-4  text-sm ">
-          <div className="flex justify-center items-center bg-red-700  w-[1000px] h-[800px]">
-            <div className="flex-row bg-indigo-800 w-[50%] h-[600px]">
+      <div className="flex-col  ml-[200px] mt-[90px]  mr-10 justify-center ">
+        <Link to={{ pathname: '/homepage' }}>
+          <div className="flex mb-6">
+            <a className="text-blue-600 cursor-pointer no-underline hover:underline text-sm ">
+              Return to Netflix
+            </a>
+          </div>
+        </Link>
+        <div className=" mb-4  text-sm mt-5 ">
+          <div className="flex justify-center    w-[1000px] h-[800px]">
+            <div className="flex-row  w-[50%] h-[600px] pr-5">
               <div className="flex-col items-center">
                 <div className="flex justify-center flex-col mb-5">
                   <h1 className="text-2xl font-semibold mb-3">
@@ -28,11 +32,11 @@ export default function CancelMemberShip() {
                   </p>
                 </div>
                 <div className="flex justify-center ">
-                  <form className="bg-white w-[95%] h-[50%] p-5 shadow-xl">
+                  <form className="bg-white w-[95%] h-[50%] p-5 shadow-xl border-gray-600">
                     <div className="flex-start ">
                       <ul>
                         <li>
-                          Canceellation will be effective at the end of your
+                          Cancellation will be effective at the end of your
                           current{' '}
                         </li>
                       </ul>
@@ -54,51 +58,140 @@ export default function CancelMemberShip() {
                     <div className=" flex-col">
                       <div className="flex">
                         <button
-                          className="bg-blue-600 w-full h-8 inline-block text-white font-medium text-lg p-5 mr-1 rounded-sm shadow-lg"
+                          className="bg-blue-600 w-full h-8 inline-block text-white font-light text-lg  mr-1 rounded-sm shadow-lg"
                           type="button"
                         >
                           Finish Cancellation
                         </button>
+                        {/* <Link to={{ pathname: '/editaccountpage' }}> */}
                         <button
-                          className="bg-gray-400 w-full h-8 inline-block text-black font-medium text-lg p-5 ml-1 rounded-sm shadow-lg"
+                          className="bg-gray-200 w-full h-8 inline-block text-black font-medium text-lg  ml-1 rounded-sm shadow-lg"
                           type="button"
                         >
                           Go Back
                         </button>
+                        {/* </Link> */}
                       </div>
                       <br />
                       <div className="flex justify-center space-x-2">
                         <button
-                          className="bg-gray-400 w-full h-8 text-black font-medium text-lg p-5"
+                          className="bg-gray-200 w-full h-8 text-black font-medium text-lg "
                           type="button"
                         >
                           Change to Basic
                         </button>
                       </div>
+                      <div className="flex justify-center items-center flex-col p-4 mt-3">
+                        <div className="flex">
+                          <h1 className="text-lg text-gray-400 font-semibold ">
+                            THB 149/Month
+                          </h1>
+                          <div className="w-[20px] h-[20px] fill-gray-400 mx-4 mt-2">
+                            <ArrowRightForm />
+                          </div>
+                          <h1 className="text-lg text-black font-semibold">
+                            THB 133/Month
+                          </h1>
+                        </div>
+                        <div className="flex justify-center items-center  mt-5">
+                          <p className="text-sm ">
+                            Better video quality in HD [1080p] for 3 Months.
+                            Watch on any phone,computer or TV.
+                          </p>
+                        </div>
+                        <Link to={{ pathname: '/signup/package' }}>
+                          <div className="mt-8">
+                            <a className="text-blue-600 cursor-pointer no-underline hover:underline">
+                              Compare All Plans
+                            </a>
+                          </div>
+                        </Link>
+                      </div>
                     </div>
                   </form>
                 </div>
-                <div className="flex justify-center items-center flex-col p-4">
-                  <div className="flex">
-                    <h1 className="text-lg text-gray-400 font-semibold ">
-                      THB 149/Month
-                    </h1>
-                    <div className="w-[20px] h-[20px] fill-gray-400 mx-4">
-                      <ArrowRightForm />
-                    </div>
-                    <h1 className="text-md text-black">THB 133/Month</h1>
+              </div>
+            </div>
+            <div className="flex-row  w-[50%] h-[600px] pl-5">
+              <div className="flex justify-center flex-col">
+                <h1 className="text-2xl font-semibold mb-4">
+                  Checkout this TV shows & movies before you leave
+                </h1>
+                <div className="grid grid-cols-4  gap-x-2 gap-y-2 ">
+                  <img
+                    src={MovieJunge}
+                    className="w-[90px] h-[120px] rounded-md flex "
+                  />
+                  <img
+                    src={MovieJunge}
+                    className="w-[90px] h-[120px] rounded-md "
+                  />
+                  <img
+                    src={MovieJunge}
+                    className="w-[90px] h-[120px] rounded-md "
+                  />
+                  <img
+                    src={MovieJunge}
+                    className="w-[90px] h-[120px] rounded-md"
+                  />
+                  <img
+                    src={MovieJunge}
+                    className="w-[90px] h-[120px] rounded-md"
+                  />
+                  <img
+                    src={MovieJunge}
+                    className="w-[90px] h-[120px] rounded-md"
+                  />
+                  <img
+                    src={MovieJunge}
+                    className="w-[90px] h-[120px] rounded-md"
+                  />
+                  <img
+                    src={MovieJunge}
+                    className="w-[90px] h-[120px] rounded-md"
+                  />
+                  <img
+                    src={MovieJunge}
+                    className="w-[90px] h-[120px] rounded-md"
+                  />
+                  <img
+                    src={MovieJunge}
+                    className="w-[90px] h-[120px] rounded-md"
+                  />
+                  <img
+                    src={MovieJunge}
+                    className="w-[90px] h-[120px] rounded-md"
+                  />
+                  <img
+                    src={MovieJunge}
+                    className="w-[90px] h-[120px] rounded-md"
+                  />
+                </div>
+                <Link to={{ pathname: '/homepage' }}>
+                  <div className="flex justify-center items-center mt-8 ">
+                    <button className="bg-gray-200 w-[70%] h-8 inline-block text-black font-medium text-lg  ml-1 rounded-sm shadow-lg">
+                      Explore More TV Shows & Movies
+                    </button>
                   </div>
-                  <p className="text-sm">
-                    Good video quality in HD [720] for 3 Months. Wattch on any
-                    phone, tablet, computer or TV.
-                  </p>
-                  <a className="text-blue-600 cursor-pointer no-underline hover:underline">
-                    Compare All Plans
-                  </a>
+                </Link>
+                <div className="flex justify-center items-center mt-5">
+                  <form className="bg-white w-[95%] h-[50%] p-5 shadow-xl">
+                    <div className="flex justify-center ">
+                      <p classname="text-black-400 font-semibold text-2xl">
+                        Want to Stick?
+                      </p>
+                    </div>
+                    <Link to={{ pathname: '/homepage' }}>
+                      <div className="flex justify-center space-x-2 mt-3">
+                        <button className="bg-blue-600 w-[40%] h-8 inline-block text-white font-light text-lg  mr-1 rounded-sm shadow-lg">
+                          Return to Netflix{' '}
+                        </button>
+                      </div>
+                    </Link>
+                  </form>
                 </div>
               </div>
             </div>
-            <div className="flex-row bg-green-500 w-[50%] h-[600px]"></div>
           </div>
         </div>
       </div>
